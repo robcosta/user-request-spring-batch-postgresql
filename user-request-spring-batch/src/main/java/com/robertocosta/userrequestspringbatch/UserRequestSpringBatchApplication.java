6cd5104abf004f8301dd4b3916e602a364cb55b4
Roberto Costa <robertocosta66@gmail.com>
2024-05-14 16:04:14 -0300

    Initial commit

---
+++ user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/UserRequestSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.robertocosta.userrequestspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestSpringBatchApplication.class, args);
+	}
+
+}
---
+++ user-request-spring-batch/src/test/java/com/robertocosta/userrequestspringbatch/UserRequestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertocosta.userrequestspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
