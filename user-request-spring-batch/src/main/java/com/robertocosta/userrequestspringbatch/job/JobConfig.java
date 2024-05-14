dd2a35625ca9bbb500162d666d2769522de5cab4
Roberto Costa <robertocosta66@gmail.com>
2024-05-14 16:06:38 -0300

    Job configuration

---
+++ user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/job/JobConfig.java
@@ -0,0 +1,28 @@
+package com.robertocosta.userrequestspringbatch.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobConfig {
+
+	private static Logger logger = LoggerFactory.getLogger(JobConfig.class);
+	
+	@Bean
+	public Job job(JobRepository jobRepository, Step facthUserDataAndStoreDBStep) {
+		
+		logger.info("Start job execution ...");
+		
+		return new JobBuilder("job", jobRepository)
+				.start(facthUserDataAndStoreDBStep)
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+}
