32c90277dbde300db6ba407eeadc2af221dcb8c3
Roberto Costa <robertocosta66@gmail.com>
2024-05-14 16:07:28 -0300

    Step configuration

---
+++ user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/step/FacthUserDataAndStoreDBStepConfig.java
@@ -0,0 +1,32 @@
+package com.robertocosta.userrequestspringbatch.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.robertocosta.userrequestspringbatch.dto.UserDTO;
+
+@Configuration
+public class FacthUserDataAndStoreDBStepConfig {
+	
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+
+	@Value("${chunckSize)")
+	private int chunkSize;
+	
+	@Bean
+	public Step facthUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader,JobRepository jobRepository) {
+		
+		return new StepBuilder("facthUserDataAndStoreDBStep", jobRepository)
+				.<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+				.reader(fetchUserDataReader)
+				.build();
+	}
+}
