feb0c6cd7e6af8d0ab9e3b6116ea2fe99c6029c9
Roberto Costa <robertocosta66@gmail.com>
2024-05-17 12:28:55 -0300

    Chunk Writer - Ok!

--- user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/config/DataSourceConfig.java
+++ user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.robertocosta.userrequestspringbatch.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class DataSourceConfig {
--- user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/writer/InsertUserDataDBWriterConfig.java
+++ user-request-spring-batch/src/main/java/com/robertocosta/userrequestspringbatch/writer/InsertUserDataDBWriterConfig.java
@@ -1,6 +1,13 @@
 package com.robertocosta.userrequestspringbatch.writer;
 
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +15,17 @@ import com.robertocosta.userrequestspringbatch.entities.User;
 
 @Configuration
 public class InsertUserDataDBWriterConfig {
+	
+	private static Logger logger = LoggerFactory.getLogger(InsertUserDataDBWriterConfig.class);
 
 	@Bean
-	ItemWriter<User> insertUserDataDBWriter() {
-		return users -> users.forEach(System.out::println);
+	ItemWriter<User> insertUserDataDBWriter(@Qualifier("appDB") DataSource dataSource) {
+		logger.info("[WRITER STEP] Insert user data ...");
+		
+		return new JdbcBatchItemWriterBuilder<User>()
+				.dataSource(dataSource)
+				.sql("INSERT INTO tb_user(login, name, avatar_url) VALUES(:login, :name, :avatarUrl)")
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.build();
 	}
 }
